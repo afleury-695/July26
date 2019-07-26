960ef5d9944b02eae37c1fd5a994f9244a68463d
Cecilia Ames <afleury@argyletech.net>
2019-07-26 09:50:29 -0500

    Edit 4

--- src/main/java/july26efforttwo.java
+++ src/main/java/july26efforttwo.java
@@ -6,5 +6,6 @@ public class july26efforttwo {
         System.out.println("making another effort to commit changes");
         System.out.println("Making another round of changes to commit (2)");
         System.out.println("Edit 3");
+        System.out.println("Edit 4");
     }
 }
